403bcfa6bf94d79d49ec34ff7de80011fa83cd2c
jar32 <javier.almagro@goumh.umh.es>
2020-05-02 02:21:48 +0200

    webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import './assets/sass/style.scss';
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
