4a95981a3ec1e89ea9a0b36c17e839a587d38508
jar32 <javier.almagro@goumh.umh.es>
2020-05-22 20:48:14 +0200

    doofinder integration

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -10,3 +10,23 @@ jQuery(".view-more").click(function () {
     var id = aux[2];
     jQuery("#view-more-block-"+id).slideToggle();
 })
+
+
+/***************************************
+ * Doofinder Integration
+ **************************************/
+var doofinder_script ='//cdn.doofinder.com/media/js/doofinder-classic.7.latest.min.js';
+(function(d,t){var f=d.createElement(t),s=d.getElementsByTagName(t)[0];f.async=1;
+f.src=('https:'==location.protocol?'https:':'http:')+doofinder_script;
+f.setAttribute('charset','utf-8');
+s.parentNode.insertBefore(f,s)}(document,'script'));
+
+var dfClassicLayers = [{
+  "hashid": "b84241003e0def6e43a4416858780ecb",
+  "zone": "eu1",
+  "display": {
+    "lang": "es"
+  },
+  "queryInput": "input[name='s']",
+  "toggleInput": "input[name='s']"
+}];
